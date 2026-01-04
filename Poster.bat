fd0f4ec7477b386df760cb10439d6f9e796b1d40
JiuBeixin <jiubeixin@gmail.com>
2026-01-04 18:37:16 +0800

    return

--- Poster.bat
+++ Poster.bat
@@ -4,13 +4,19 @@ set https_proxy=http://127.0.0.1:7890
 
 
 @echo off
-
 set URL=https://school2family.vercel.app/api/index.php
-echo Sending POST request to %URL%...
+
+:: 模拟真实数据的变量
+set TO_USER=gh_f8c123456789
+set FROM_USER=oUp6SjvY2b2AN96_1Z8_SAMPLE
+set CONTENT=你好，我想咨询
+
+echo Sending simulated user message to %URL%...
 
 curl -X POST %URL% ^
 -H "Content-Type: text/xml" ^
--d "<xml><ToUserName><![CDATA[gh_test]]></ToUserName><FromUserName><![CDATA[tester]]></FromUserName><CreateTime>123456</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[Hello Vercel]]></Content><MsgId>123456789</MsgId></xml>"
+-d "<xml><ToUserName><![CDATA[%TO_USER%]]></ToUserName><FromUserName><![CDATA[%FROM_USER%]]></FromUserName><CreateTime>%date:~0,4%%date:~5,2%%date:~8,2%</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%CONTENT%]]></Content><MsgId>23948576123456789</MsgId></xml>"
 
 echo.
-echo Request sent. Check your Vercel Logs.
+echo Done! Check Vercel Logs for the XML response.
+pause
